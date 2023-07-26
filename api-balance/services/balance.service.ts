d20726528076679c3d7140483b22c20feb75fb6c
Christopher Siverio <christopher.siverio@coppel.com>
2023-07-26 15:23:25 -0700

    FIX: :bug: Se ajusta respuesta de assets

--- api-balance/services/balance.service.ts
+++ api-balance/services/balance.service.ts
@@ -1,18 +1,14 @@
 import axios from 'axios';
 
 export default class BalanceService {
-  private readonly apiUrl: string = 'https://data.messari.io/api/v1/';
+  private readonly apiUrl: string = 'https://data.messari.io/api/v1';
 
   async getBalance(crypto: String): Promise<Object> {
 
     let url: string = `${this.apiUrl}/assets/${crypto}/metrics`;
-
     const response = await axios.get(url);
 
-    return response;
+    return response.data;
   }
 
-
-  
-  
 }
